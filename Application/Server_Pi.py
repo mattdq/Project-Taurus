e7b0e00594e8613a7955471ab175104d30d29e68
mattdq <matheusantunesmvs@gmail.com>
2021-01-14 10:38:34 -0300

    v0.0.6 - Changes trying to make flask work on gpiozero.

--- Application/Server_Pi.py
+++ Application/Server_Pi.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 from flask_restplus import Api, Resource, fields
 from werkzeug.contrib.fixers import ProxyFix
 
-from gpiozero import LED
+import RPi.GPIO as GPIO
 
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app)
@@ -10,13 +10,19 @@ api = Api(app, version='1.0', title='Taurus API', description='First Taurus API'
 
 ha = api.namespace('HomeAutomation', description = 'Methods to control the house.')
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+led = 17
+ledsts = 0
+GPIO.setup(led, GPIO.OUT)
+GPIO.output(led, GPIO.LOW)
+
 
 @ha.route('/turnledon', methods=['GET'])
 class TurnLedOn(Resource):
     @ha.doc('Turn led on.')
     def get(self):
-        led = LED(17)
-        led.on()
+        GPIO.output('led', GPIO.HIGH)
         return {"Message": "Turned on."}, 200
 
 
@@ -24,8 +30,7 @@ class TurnLedOn(Resource):
 class TurnLedOff(Resource):
     @ha.doc('Turn led off.')
     def get(self):
-        led = LED(17)
-        led.off()
+        GPIO.output('led', GPIO.LOW)
         return {"Message": "Turned off."}, 200
 
 
